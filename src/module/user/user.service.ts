ce61f225c5d7a543e7a569dd3a6a01abf72151b7
Oladimeji Ajeniya <oladimejiajeniya@gmail.com>
2024-03-23 19:14:47 +0100

    update time helper

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -64,9 +64,12 @@ export class UsersService {
   }
 
   private formatDate(date: Date): string {
+    if (!date) {
+      return ''; 
+    }
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Ensure two digits for month
-    const day = String(date.getDate()).padStart(2, '0'); // Ensure two digits for day
+    const month = String(date.getMonth() + 1).padStart(2, '0'); 
+    const day = String(date.getDate()).padStart(2, '0'); 
     return `${year}-${month}-${day}`;
   }
 }
